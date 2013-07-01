1f30b22df906f5ef75a4edea84f38bde8cb9ac43
James Patton <jamesyp@gmail.com>
2013-07-01 17:14:34 -0600

    Added site, variable to xml export

--- src/odmdata/site.py
+++ src/odmdata/site.py
@@ -8,8 +8,8 @@ class Site(Base):
 	__tablename__ = 'Sites'
 
 	id 					= Column('SiteID', Integer, primary_key=True)
-	site_code 		    = Column('SiteCode', String)
-	site_name 		    = Column('SiteName', String)
+	code 			    = Column('SiteCode', String)
+	name 	 		    = Column('SiteName', String)
 	latitude  		    = Column('Latitude', Float)
 	longitude			= Column('Longitude', Float)
 	lat_long_datum_id   = Column('LatLongDatumID', Integer, ForeignKey('SpatialReferences.SpatialReferenceID'))
@@ -17,16 +17,17 @@ class Site(Base):
 	vertical_datum_id   = Column('VerticalDatum', Integer)
 	local_x			    = Column('LocalX', Float)
 	local_y			    = Column('LocalY', Float)
-	local_projection_id = Column('LocalProjectionID', Integer)
+	local_projection_id = Column('LocalProjectionID', Integer, ForeignKey('SpatialReferences.SpatialReferenceID'))
 	pos_accuracy_m	    = Column('PosAccuracy_m', Float)
 	state			    = Column('State', String)
 	county			    = Column('County', String)
 	comments		    = Column('Comments', String)
 
-	site_type			= Column('SiteType', String)
+	type			= Column('SiteType', String)
 
 	# relationships
-	spatial_reference = relationship(SpatialReference, primaryjoin=("SpatialReference.id==Site.lat_long_datum_id"))
+	spatial_ref = relationship(SpatialReference, primaryjoin=("SpatialReference.id==Site.lat_long_datum_id"))
+	local_spatial_ref = relationship(SpatialReference, primaryjoin=("SpatialReference.id==Site.local_projection_id"))
 
 	def __init__(self, site_code, site_name):
 		self.site_code = site_code
--- src/odmservices/export_service.py
+++ src/odmservices/export_service.py
@@ -33,10 +33,10 @@ class ExportService():
 		data.append(series.site_code)
 		if site:
 			data.append(series.site_name)
-			data.append(series.site.site_type)
+			data.append(series.site.type)
 			data.append(series.site.latitude)
 			data.append(series.site.longitude)
-			data.append(series.site.spatial_reference.srs_name)
+			data.append(series.site.spatial_ref.srs_name)
 		data.append(series.variable_code)
 		if var:
 			data.append(series.variable_name)
@@ -128,7 +128,7 @@ class ExportService():
 
 		root = ET.Element("Metadata")
 		list_root = ET.SubElement(root, "DataSeriesList")
-		list_root.set("Total", len(series_ids))
+		list_root.set("Total", str(len(series_ids)))
 
 		try:
 			with open(filename): file_exists = True
@@ -141,12 +141,129 @@ class ExportService():
 		
 		for series_id in series_ids:
 			series = self._series_service.get_series_by_id(series_id)
-			site = series.site
-			variable = series.variable
-			method = series.method
-			source = series.source
-			qcl = series.quality_control_level
-			offsets = self._series_service.get_offset_types_by_series_id(series.id)
-			qualifiers = self._series_service.get_qualifiers_by_series_id(series.id)
-			samples = self._series_service.get_samples_by_series_id(series.id)
+			self.append_series_node(series, list_root)
 
+		tree = ET.ElementTree(root)
+		tree.write(filename)
+
+	def append_series_node(self, series, parent):
+		series_node = ET.SubElement(parent, "DataSeries")
+		series_node.set("ID", str(series.id))
+		self.append_general_info(series, series_node)
+		self.append_site_info(series, series_node)
+		self.append_var_info(series, series_node)
+
+		return series_node
+		
+	def append_general_info(self, series, parent):
+		meta = series.source.iso_metadata
+		general_node = ET.SubElement(parent, "GeneralInformation")
+		topic = ET.SubElement(general_node, "TopicCategory")
+		topic.text = meta.topic_category
+		title = ET.SubElement(general_node, "Title")
+		title.text = meta.title
+		abstract = ET.SubElement(general_node, "Abstract")
+		abstract.text = meta.abstract
+		prof_version = ET.SubElement(general_node, "ProfileVersion")
+		prof_version.text = meta.profile_version
+		metadata_link = ET.SubElement(general_node, "MetadataLink")
+		metadata_link.text = meta.metadata_link
+		date = ET.SubElement(general_node, "MetadataCreationDate")
+		# 7/1/2013 12:17:16 PM
+		import datetime
+		date.text = datetime.datetime.now().strftime("%m/%d/%Y %I:%M:%S %p")
+
+	def append_site_info(self, series, parent):
+		site = series.site
+		site_node = ET.SubElement(parent, "SiteInformation")
+		site_code = ET.SubElement(site_node, "SiteCode")
+		site_code.text = site.code
+		site_name = ET.SubElement(site_node, "SiteName")
+		site_name.text = site.name
+		site_type = ET.SubElement(site_node, "SiteType")
+		site_type.text = site.type
+
+		geo_coords = ET.SubElement(site_node, "GeographicCoordinates")
+		latitude = ET.SubElement(geo_coords, "Latitude")
+		latitude.text = str(site.latitude)
+		longitude = ET.SubElement(geo_coords, "Longitude")
+		longitude.text = str(site.longitude)
+		srs_id = ET.SubElement(geo_coords, "SRSID")
+		srs_id.text = str(site.spatial_ref.srs_id)
+		srs_name = ET.SubElement(geo_coords, "SRSName")
+		srs_name.text = site.spatial_ref.srs_name
+		is_geo = ET.SubElement(geo_coords, "IsGeographic")
+		is_geo.text = str(site.spatial_ref.is_geographic)
+		notes = ET.SubElement(geo_coords, "Notes")
+		notes.text = site.spatial_ref.notes
+		
+		local_coords = ET.SubElement(site_node, "LocalCoordinates")
+		local_x = ET.SubElement(local_coords, "LocalX")
+		local_x.text = str(site.local_x)
+		local_y = ET.SubElement(local_coords, "LocalY")
+		local_y.text = str(site.local_y)
+		local_srs_id = ET.SubElement(local_coords, "SRSID")
+		local_srs_id.text = str(site.local_spatial_ref.srs_id)
+		local_srs_name = ET.SubElement(local_coords, "SRSName")
+		local_srs_name.text = site.local_spatial_ref.srs_name
+		local_is_geo = ET.SubElement(local_coords, "IsGeographic")
+		local_is_geo.text = str(site.local_spatial_ref.is_geographic)
+		local_notes = ET.SubElement(local_coords, "Notes")
+		local_notes.text = site.local_spatial_ref.notes
+		elevation = ET.SubElement(local_coords, "Elevation_m")
+		if site.elevation_m: elevation.text = str(site.elevation_m)
+		vert_datum = ET.SubElement(local_coords, "VerticalDatum")
+		if site.vertical_datum_id: vert_datum.text = str(site.vertical_datum_id)
+
+		pos_accuracy = ET.SubElement(site_node, "PosAccuracy_m")
+		pos_accuracy.text = str(site.pos_accuracy_m)
+		state = ET.SubElement(site_node, "State")
+		state.text = site.state
+		county = ET.SubElement(site_node, "County")
+		county.text = site.county
+		comments = ET.SubElement(site_node, "Comments")
+		comments.text = site.comments
+
+	def append_var_info(self, series, parent):
+		variable = series.variable 
+		var_node = ET.SubElement(parent, "VariableInformation")
+
+		var_code = ET.SubElement(var_node, "VariableCode")
+		var_code.text = variable.code
+		var_name = ET.SubElement(var_node, "VariableName")
+		var_name.text = variable.name
+		speciation = ET.SubElement(var_node, "Speciation")
+		speciation.text = variable.speciation
+
+		var_units = ET.SubElement(var_node, "VariableUnits")
+		units_name = ET.SubElement(var_units, "UnitsName")
+		units_name.text = variable.variable_unit.name
+		units_type = ET.SubElement(var_units, "UnitsType")
+		units_type.text = variable.variable_unit.type
+		units_abbrev = ET.SubElement(var_units, "UnitsAbbreviation")
+		units_abbrev.text = variable.variable_unit.abbreviation
+
+		sample_medium = ET.SubElement(var_node, "SampleMedium")
+		sample_medium.text = variable.sample_medium
+		val_type = ET.SubElement(var_node, "ValueType")
+		val_type.text = variable.value_type
+		is_reg = ET.SubElement(var_node, "IsRegular")
+		is_reg.text = str(variable.is_regular)
+		time_support = ET.SubElement(var_node, "TimeSupport")
+		time_support.text = str(variable.time_support)
+
+		time_support_units = ET.SubElement(var_node, "TimeSupportUnits")
+		ts_units_name = ET.SubElement(time_support_units, "UnitsName")
+		ts_units_name.text = variable.time_unit.name
+		ts_units_type = ET.SubElement(time_support_units, "UnitsType")
+		ts_units_type.text = variable.time_unit.type
+		ts_units_abbrev = ET.SubElement(time_support_units, "UnitsAbbreviation")
+		ts_units_abbrev.text = variable.time_unit.abbreviation
+
+		data_type = ET.SubElement(var_node, "DataType")
+		data_type.text = variable.data_type
+		gen_cat = ET.SubElement(var_node, "GeneralCategory")
+		gen_cat.text = variable.general_category
+		no_dv = ET.SubElement(var_node, "NoDataValue")
+		no_dv.text = str(variable.no_data_value)
+		 
