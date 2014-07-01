6bba1f8244313e184781fdf4db6f6fd25c67d6c9
sreeder <stephanie.reeder@usu.edu>
2014-07-01 16:58:28 -0600

    resolved an additional mac issue dealing with the inability to find the user_config_dir

--- odmtools/odmservices/service_manager.py
+++ odmtools/odmservices/service_manager.py
@@ -9,6 +9,7 @@ from edit_service import EditService
 from record_service import RecordService
 from export_service import ExportService
 from appdirs import *
+import os
 
 
 tool = LoggerTool()
@@ -119,7 +120,12 @@ class ServiceManager():
 
         config_file = None
         try:
-            config_file = open(fn, mode)
+            if os.path.exists(fn):
+                config_file = open(fn, mode)
+            else:
+                os.makedirs(user_config_dir("ODMTools", "UCHIC"))
+                open(fn, 'w').close()
+                config_file = open(fn, mode)
         except:
             open(fn, 'w').close()
             config_file = open(fn, mode)
