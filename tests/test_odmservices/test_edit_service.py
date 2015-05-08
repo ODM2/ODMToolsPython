dacbfdfcba78b93aa3f99117d0118795898241b5
sreeder <stephanie.reeder@usu.edu>
2015-05-08 17:33:45 -0600

    update save tests, still need a little work

--- tests/test_odmservices/test_edit_service.py
+++ tests/test_odmservices/test_edit_service.py
@@ -18,30 +18,26 @@ class TestSeriesService:
         self.session = self.memory_database.series_service._session_factory.get_session()
 
         self.series = test_util.add_series_bulk_data(self.session)
-
-
         #assert len(self.series.data_values) == 100
 
-
         self.edit_service =EditService(1, connection= self.memory_database)
-
-
 
 
     ## TODO Unittest save_series, save_as, save_as_existing
 
     def test_save_series(self):
-        assert self.edit_service.save(self.series)
+        assert self.edit_service.save()
 
     def test_save_as_series(self):
         var = test_util.add_variable(self.session)
         print var
-        assert self.edit_service.save_as(var= var.id)
-        assert self.edit_service._series_service.series_exists(self.series.site.id, var.id, self.series.method.id,
-                                                 self.series.source.id, self.series.qcl.id)
+        assert self.edit_service.save_as(var= var)
+        ##assert self.edit_service.memDB.series_service.series_exists(self.series.site.id, var, self.series.method.id,
+        #                                         self.series.source.id, self.series.qcl.id)
 
-'''
+
     def test_save_as_existing_series(self):
-        assert not self.series_service.save_existing(series)
-'''
+        var = test_util.add_variable(self.session)
+        assert self.edit_service.save_existing(var = var)
+
 
