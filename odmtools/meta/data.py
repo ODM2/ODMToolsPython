cbff1ff1755781ff4c603e5312fc0babfda90b3a
Jacob Meline <jacob.meline@gmail.com>
2015-05-21 12:04:07 -0600

    Adding meta.py

---
+++ odmtools/meta/data.py
@@ -0,0 +1,22 @@
+app_name = "ODMTools"
+version = "1.2.0_Beta"
+copyright = "Copyright (c) 2013 - 2015, Utah State University. All rights reserved."
+description = "ODMTools is a python application for managing observational data using the Observations Data Model. " \
+            "ODMTools allows you to query, visualize, and edit data stored in an Observations Data Model (ODM) database." \
+            " ODMTools was originally developed as part of the CUAHSI Hydrologic Information System."
+
+developers = ["Jeffery S. Horsburgh", "Amber Spackman Jones",
+                "Stephanie L. Reeder", "Jacob Meline", "James Patton"]
+
+license = "This material is copyright (c) 2013 - 2015 Utah State University." \
+              "\nIt is open and licensed under the New Berkeley Software Distribution (BSD) License. Full text of the license follows." \
+              "\nCopyright (c) 2013, Utah State University. All rights reserved." \
+              "\nRedistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:" \
+              "\n    Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer." \
+              "\n    Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution." \
+              "\n    Neither the name of Utah State University nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission." \
+              "\nTHIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. "
+
+
+website = ("http://uchic.github.io/ODMToolsPython/", "ODMTools home page")
+
