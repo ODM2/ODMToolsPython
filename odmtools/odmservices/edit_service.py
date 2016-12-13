9aa05f8867248d228f794e371800e660dec31d89
sreeder <stephanie.reeder@usu.edu>
2016-12-13 16:45:53 -0700

    fix improper location of ResultID deletion

--- odmtools/odmservices/edit_service.py
+++ odmtools/odmservices/edit_service.py
@@ -551,8 +551,7 @@ class EditService():
         values = self.memDB.getDataValuesDF()
         # copy old
         result = self.memDB.series_service.get_series(str(values["resultid"][0]))
-        result.ResultID = None
-        result.ResultUUID = None
+
 
 
         # change var, meth proc, in df #intend ts, agg sta
@@ -571,6 +570,7 @@ class EditService():
         #if result does not exist
         if not self.memDB.series_service.resultExists(result):
             try:
+
                 #create Action
                 action.ActionID = None
                 action.ActionTypeCV = "Derivation"
@@ -601,6 +601,8 @@ class EditService():
                 # create TimeSeriesResult - this should also contain all of the stuff for the Result
                 time, offset = self.get_current_time_and_utcoffset()
 
+                result.ResultID = None
+                result.ResultUUID = None
                 result.ValueCount = 0
                 result.FeatureActionID = feature_action.FeatureActionID
                 result.ResultDateTime = time
