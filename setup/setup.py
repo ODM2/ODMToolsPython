577a6e60f555c85969e0ff7abd86df33bc9c1c9d
stephanie <stephanie.reeder@usu.edu>
2017-02-14 15:37:12 -0700

    run macholib only if on a mac

--- setup/setup.py
+++ setup/setup.py
@@ -11,23 +11,25 @@ Usage (Windows):
  python setup.py py2exe
 """
 
-import macholib
-#print("~"*60 + "macholib verion: "+macholib.__version__)
-if macholib.__version__ <= "1.7":
-    print("Applying macholib patch...")
-    import macholib.dyld
-    import macholib.MachOGraph
-    dyld_find_1_7 = macholib.dyld.dyld_find
-    def dyld_find(name, loader=None, **kwargs):
-        #print("~"*60 + "calling alternate dyld_find")
-        if loader is not None:
-            kwargs['loader_path'] = loader
-        return dyld_find_1_7(name, **kwargs)
-    macholib.MachOGraph.dyld_find = dyld_find
-
 
 import sys
 import os
+if sys.platform == 'darwin':
+    import macholib
+    #print("~"*60 + "macholib verion: "+macholib.__version__)
+    if macholib.__version__ <= "1.7":
+        print("Applying macholib patch...")
+        import macholib.dyld
+        import macholib.MachOGraph
+        dyld_find_1_7 = macholib.dyld.dyld_find
+        def dyld_find(name, loader=None, **kwargs):
+            #print("~"*60 + "calling alternate dyld_find")
+            if loader is not None:
+                kwargs['loader_path'] = loader
+            return dyld_find_1_7(name, **kwargs)
+        macholib.MachOGraph.dyld_find = dyld_find
+
+
 '''
 from setuptools import setup
 
